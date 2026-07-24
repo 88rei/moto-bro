c74759cb8321c16a54725c9304fa12788e5df3ec
Andre Reynaldi Lusikooy <reynaldi@outlook.jp>
2026-07-25 00:16:23 +0700

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     port: 8080,
+  },
+  resolve: {
+    tsconfigPaths: true,
   },
   plugins: [
     tanstackStart({
@@ -14,6 +16,5 @@ export default defineConfig({
     }),
     react(),
     tailwindcss(),
-    tsconfigPaths(),
   ],
 });
